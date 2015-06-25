237df1d556dcf7bc08683dd05b15fad3d502a3be
Ondrej Vana <ondrej.vana@keboola.com>
2015-06-25 13:15:21 -0700

    feat: Properly working writer

---
+++ src/Keboola/DropboxWriter/Writer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Keboola\DropboxWriter;
+
+class Writer
+{
+	
+}
--- src/run.php
+++ src/run.php
@@ -1,6 +1,9 @@
 <?php
 
 use Symfony\Component\Yaml\Yaml;
+use Dropbox\Client,
+	Dropbox\WriteMode;
+use Guzzle\Http\Client as Guzzle;
 
 require_once(dirname(__FILE__) . "/../vendor/autoload.php");
 
@@ -12,30 +15,58 @@ if (!isset($arguments["data"])) {
 
 $config = Yaml::parse(file_get_contents($arguments["data"] . "/config.yml"));
 
-var_dump($config);
-die();
+if (empty($config['parameters']['api_key'])) {
+	print "Missing configuration parameter 'api_key'!";
 
+	if (!empty($config['parameters']['credentials'])) {
 
-if (isset($config["storage"]["input"]["tables"][0]["destination"])) {
-    $sourceFile  = $config["storage"]["input"]["tables"][0]["destination"];
-} else {
-    $sourceFile = $config["storage"]["input"]["tables"][0]["source"];
+		$guzzle = new Guzzle;
+		try {
+			$token = getenv('KBC_TOKEN');
+			if (empty($token)) {
+				print 'KBC_TOKEN env variable not set.';
+				exit(1);
+			}
+
+			$re = $guzzle->get(
+				"https://syrup-testing.keboola.com/oauth/get/wr-dropbox/{$config['parameters']['credentials']}",
+				['X-StorageApi-Token' => $token]
+			);
+		} catch(\Guzzle\Http\Exception\RequestException $e) {
+			print "Failed retrieving token from OAuth API: " . $e->getMessage();
+			exit($e->getCode() ?: 1);
+		}
+
+	} else {
+		print "'api_key' or 'credentials' parameter is required";
+		exit(2);
+	}
 }
-$destinationFile = "sliced.csv";
 
-try {
-    $splitter = new \Keboola\DockerDemo\Splitter();
-    $rows = $splitter->processFile(
-        $arguments["data"] . "/in/tables/{$sourceFile }",
-        $arguments["data"] . "/out/tables/{$destinationFile}",
-        $config["parameters"]["primary_key_column"],
-        $config["parameters"]["data_column"],
-        $config["parameters"]["string_length"]
-    );
-} catch (\Keboola\DockerDemo\Splitter\Exception $e) {
-    print $e->getMessage();
-    exit(1);
+if (empty($config['storage']['input']['tables'])) {
+	print "No input data found!";
+	exit(1);
 }
 
-print "Processed {$rows} rows.";
+$client = new Client($config['parameters']['api_key'], "Keboola Dropbox Writer/0.1");
+
+$path = empty($config['parameters']['path_prefix']) ? "" : $config['parameters']['path_prefix'];
+
+$mode = WriteMode::add(); // TODO configurable
+
+foreach($config['storage']['input']['tables'] as $table) {
+// TODO check if destination ain't empty
+	$result = $client->uploadFile(
+		'/' . $path . $table['destination'],
+		$mode,
+		fopen($arguments["data"] . "/in/tables/{$table['destination']}", 'r')
+	);
+	print "Uploaded to {$result['path']}" . PHP_EOL;
+}
+
+// if (isset($config["storage"]["input"]["tables"][0]["destination"])) {
+//     $sourceFile  = $config["storage"]["input"]["tables"][0]["destination"];
+// } else {
+//     $sourceFile = $config["storage"]["input"]["tables"][0]["source"];
+// }
 exit(0);
